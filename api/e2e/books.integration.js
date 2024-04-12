c7079267714089292c83642b68223cb70448289c
yleal <63595588+yleal@users.noreply.github.com>
2024-04-11 20:12:32 -0400

    add github actions

---
+++ api/e2e/books.integration.js
@@ -0,0 +1,40 @@
+const mockGetAll = jest.fn(); // Por alguna razón se debe inicializar el mock antes que nada
+const request = require('supertest');
+
+const createApp = require('../src/app');
+const { generateManyBook } = require('../src/fakes/book.fake');
+
+jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
+  getAll: mockGetAll,
+  create: () => {},
+})));
+
+describe('Test for books', () => {
+  let app = null;
+  let server = null;
+  beforeAll(() => {
+    app = createApp();
+    server = app.listen(3001);
+  });
+
+  afterAll(async () => {
+    await server.close();
+  });
+
+  describe('test for [GET] /api/v1/books', () => {
+    test('should return a list books', () => {
+      // Arrange
+      const fakeBooks = generateManyBook(3);
+      mockGetAll.mockResolvedValue(fakeBooks);
+      // Act
+      return request(app)
+        .get('/api/v1/books')
+        .expect(200)
+        .then(({ body }) => {
+          console.log(body);
+          // Assert
+          expect(body.length).toEqual(fakeBooks.length);
+        });
+    });
+  });
+});
